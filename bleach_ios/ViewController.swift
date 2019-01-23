b25df8330acb1b601305d98bb4115210f9576766
molder <molder1007@gmail.com>
2019-01-23 10:39:41 +0800

    Initial Commit

---
+++ bleach_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bleach_ios
+//
+//  Created by Molder on 2019/1/23.
+//  Copyright © 2019 Molder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
